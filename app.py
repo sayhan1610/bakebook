78b8332d1bd9ff426ff4cedfa20990455444ff8f
Sayhan Rahman <sayhanrahman@gokkusagi.k12.tr>
2025-01-20 19:50:47 +0300

    cors update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
